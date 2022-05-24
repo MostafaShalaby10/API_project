992cf78fc8669c60df568126b1f63bccbb80819e
manon-allah <71779279+manon-allah@users.noreply.github.com>
2022-05-24 16:27:15 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const mongo = require('mongoose')
+const bodyparser = require('body-parser')
+const mid = require('./middleware')
+require('dotenv/config')
+
+mongo.connect(process.env.url , ()=>
+{
+    console.log("Connect succesffully to database")
+})
+
+const app = express();
+app.use(bodyparser.json())
+app.use(mid)
+
+
+
+app.listen(8989 , ()=>
+{
+    console.log("Server Running.........")
+})
+
